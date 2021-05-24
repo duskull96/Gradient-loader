9d60623f9a516d4d56c1901ab606d0b5e474c388
duskull96 <pavel6900@gmail.com>
2021-05-24 19:04:13 +0300

    remove func createAnim

--- gradient-load.js
+++ gradient-load.js
@@ -5,42 +5,13 @@ const bodyBG = document.querySelector('body'),
     speed = document.querySelector('#speed'),
     stepLabelValue = document.querySelector('#step-value'),
     speedLabelValue = document.querySelector('#speed-value'),
-    linearGradient = document.querySelector('#linear-gradient'),
-    copyAnim = document.querySelector('#copy-animation');
+    linearGradient = document.querySelector('#linear-gradient');
 
 const gradientDefault = '#f64f59, #c471ed, #12c2e9'; 
 let gradient = gradientDefault
 let angle = 0;
 stepLabelValue.textContent = step.value;
 speedLabelValue.textContent = speed.value / 100 + 's';
-
-function createAnimation() {
-
-    let animation = `
-    -webkit-animation: ${((360-step.value)/step.value)*speed.value*10}ms linear 0s infinite  bgAnim;
-    -moz-animation: ${((360-step.value)/step.value)*speed.value*10}ms linear 0s infinite  bgAnim;
-    -o-animation: ${((360-step.value)/step.value)*speed.value*10}ms linear 0s infinite  bgAnim;
-    animation: ${((360-step.value)/step.value)*speed.value*10}ms linear 0s infinite  bgAnim;
-
-    @-webkit-keyframes bgAnim { 
-        from { background: linear-gradient(0deg, ${gradient}); } 
-        to { background: linear-gradient(${360-step.value}deg, ${gradient}); }  
-    }
-    @-moz-keyframes bgAnim { 
-        from { background: linear-gradient(0deg, ${gradient}); } 
-        to { background: linear-gradient(${360-step.value}deg, ${gradient}); }  
-    }
-    @-o-keyframes bgAnim { 
-        from { background: linear-gradient(0deg, ${gradient}); } 
-        to { background: linear-gradient(${360-step.value}deg, ${gradient}); }  
-    }
-    @keyframes bgAnim { 
-        from { background: linear-gradient(0deg, ${gradient}); } 
-        to { background: linear-gradient(${360-step.value}deg, ${gradient}); }  
-    }
-    `
-    navigator.clipboard.writeText(animation)
-}
 
 function updateValue(e) {
     if (e.target.id == 'step') {
@@ -74,7 +45,6 @@ let timer = setTimeout(function bgChanger() {
 }, 100)
 
 
-copyAnim.addEventListener('click', createAnimation);
 step.addEventListener('input', updateValue);
 speed.addEventListener('input', updateValue);
 linearGradient.addEventListener('input', updateValue);
